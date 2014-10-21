62c4e740256954cd49484a93783c49a6b7739b44
Ryan Oaks <roaks3@gmail.com>
2014-10-20 22:04:34 -0700

    Modified model of categories in create list view to match elastic search

--- createList.js
+++ createList.js
@@ -2,13 +2,12 @@
 angular.module('createListApp', [])
     .controller('CreateListController', ['$scope', function($scope) {
         $scope.recipes = [
-            {"_name":"Cashew Chicken and Broccoli","_ingredients":{"_ingredients":[{"_item":"broccoli","_amount":"4"},{"_item":"cashews","_amount":"1"},{"_item":"onion","_amount":"1/2"}]}}];
+            {"_name":"Cashew Chicken and Broccoli","_ingredients":{"_ingredients":[{"_item":"cucumber","_amount":"4"},{"_item":"cashews","_amount":"1"},{"_item":"onion","_amount":"1/2"}]}}];
 
         $scope.shoppingList = [];
 
         $scope.categories = [
-            {name: "Fruit", order: 1, items: ["salt", "Orange"]},
-            {name: "Meat", order: 1, items: ["Chicken", "pepper"]}];
+            {"_name": "Fruits/Vegetables", "_order": 1, "_items": ["cucumber", "pobalano chili", "lime", "asparagus", "red potatoes"] }];
 
         $scope.calculateShoppingList = function() {
             var ingredientList = [];
@@ -19,8 +18,8 @@ angular.module('createListApp', [])
             $scope.shoppingList = [];
             var leftoverList = angular.copy(ingredientList);
             angular.forEach($scope.categories, function(category) {
-                var shoppingListCategory = {name: category.name, items: []};
-                angular.forEach(category.items, function(item) {
+                var shoppingListCategory = {name: category._name, items: []};
+                angular.forEach(category._items, function(item) {
                     angular.forEach(ingredientList, function(ingredient) {
                         if (item === ingredient) {
                             shoppingListCategory.items.push(item);
