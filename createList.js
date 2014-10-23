047e95c443b2bee360bd4f2f5ea61b3ee1db5d7e
Ryan Oaks <roaks3@gmail.com>
2014-10-22 22:37:51 -0700

    Used custom classes in angular create view instead of simple json objects

--- createList.js
+++ createList.js
@@ -10,7 +10,7 @@ angular.module('createListApp', ['elasticsearch'])
     })
 
     .controller('CreateListController', function($scope, client, esFactory) {
-        $scope.recipes = [];
+        $scope.recipes = new Recipes();
         client.search({
             index: 'recipes',
             type: 'recipe',
@@ -21,17 +21,25 @@ angular.module('createListApp', ['elasticsearch'])
                 }
             }
         }).then(function (body) {
-            var hits = body.hits.hits;
-            $scope.recipes = hits.map(function(element) {
-                return element._source;
-            });
+            var hitsJson = body;
+            for (var i in hitsJson.hits.hits) {
+                var recipeJson = hitsJson.hits.hits[i]._source;
+                var name = recipeJson._name;
+                var recipe = new Recipe(name, null, recipeJson._originalText);
+                for (var j in recipeJson._ingredients._ingredients) {
+                    var ingredientJson = recipeJson._ingredients._ingredients[j];
+                    var ingredient = new Ingredient(ingredientJson._item, ingredientJson._amount);
+                    recipe.addIngredient(ingredient);
+                }
+                $scope.recipes.add(recipe);
+            }
         }, function (error) {
             console.trace(error.message);
         });
 
         $scope.shoppingList = [];
 
-        $scope.categories = [];
+        $scope.categories = new Categories();
         client.search({
             index: 'recipes',
             type: 'category',
@@ -42,30 +50,39 @@ angular.module('createListApp', ['elasticsearch'])
                 }
             }
         }).then(function (body) {
-            var hits = body.hits.hits;
-            $scope.categories = hits.map(function(element) {
-                return element._source;
-            });
+            var hitsJson = body;
+            for (var i in hitsJson.hits.hits) {
+                var categoryJson = hitsJson.hits.hits[i]._source;
+                var name = categoryJson._name;
+                var order = categoryJson._order;
+                var category = new Category(name, order, null);
+                for (var j in categoryJson._items) {
+                    var item = categoryJson._items[j];
+                    category.addItem(item);
+                }
+                $scope.categories.add(category);
+            }
         }, function (error) {
             console.trace(error.message);
         });
 
         $scope.calculateShoppingList = function() {
-            var ingredientList = [];
-            angular.forEach($scope.recipes, function(recipe) {
-                if (recipe._selected) ingredientList.push(recipe._ingredients._ingredients[0]._item);
+            var ingredientList = new Ingredients();
+            angular.forEach($scope.recipes._recipes, function(recipe) {
+                if (recipe._selected) ingredientList.combineAll(recipe.getIngredients());
             });
 
             $scope.shoppingList = [];
-            var leftoverList = angular.copy(ingredientList);
-            angular.forEach($scope.categories, function(category) {
+            var leftoverList = ingredientList._ingredients.map(function(ingredient) {
+                return ingredient._item;
+            });
+            angular.forEach($scope.categories._categories, function(category) {
                 var shoppingListCategory = {name: category._name, items: []};
                 angular.forEach(category._items, function(item) {
-                    angular.forEach(ingredientList, function(ingredient) {
-                        if (item === ingredient) {
-                            shoppingListCategory.items.push(item);
-                        }
-                    });
+                    var ingredient = ingredientList.getByItem(item);
+                    if (ingredient != null) {
+                        shoppingListCategory.items.push(item);
+                    }
                     leftoverList = leftoverList.filter(function(element) {
                         return !(element === item);
                     });
