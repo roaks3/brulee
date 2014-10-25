36385644e0581565ea6c14db64fd1a8e42fc3106
Ryan Oaks <roaks3@gmail.com>
2014-10-25 16:34:29 -0700

    Added basic addRecipe view rewritten with angular

---
+++ addRecipes.js
@@ -0,0 +1,28 @@
+
+angular.module('addRecipesApp', ['elasticsearch'])
+
+    .service('client', function (esFactory) {
+        return esFactory({
+            host: 'localhost:9200',
+            apiVersion: '1.2',
+            log: 'trace'
+        });
+    })
+
+    .controller('AddRecipesController', function($scope, client, esFactory) {
+        $scope.recipe = new Recipe("", new Ingredients(), "");
+
+        $scope.addRecipe = function() {
+            client.create({
+                index: 'recipes2',
+                type: 'recipe',
+                body: $scope.recipe
+            }, function (error, response) {
+                console.trace(error.message);
+            });
+        };
+
+        $scope.parseRecipeText = function() {
+            $scope.recipe.ingredients = Ingredients.parse($scope.recipe.originalText);
+        };
+    });
