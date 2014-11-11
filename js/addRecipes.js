329c3da90373caf34a8277e62169671470c40758
Ryan Oaks <roaks3@gmail.com>
2014-11-10 21:34:53 -0800

    Added functionality to set category for new items

--- js/addRecipes.js
+++ js/addRecipes.js
@@ -16,6 +16,8 @@ angular.module('addRecipesApp', ['elasticsearch', 'ui.bootstrap'])
 
         $scope.categoryMap = {};
         $scope.items = [];
+        $scope.categoryNames = [];
+        $scope.categories = [];
         client.search({
             index: 'test',
             type: 'category',
@@ -26,7 +28,7 @@ angular.module('addRecipesApp', ['elasticsearch', 'ui.bootstrap'])
                 }
             }
         }).then(function (body) {
-            var categories = [];
+            var categories = {};
             var hitsJson = body;
             for (var i in hitsJson.hits.hits) {
                 var categoryJson = hitsJson.hits.hits[i]._source;
@@ -35,13 +37,39 @@ angular.module('addRecipesApp', ['elasticsearch', 'ui.bootstrap'])
                     var item = categoryJson.items[j];
                     $scope.categoryMap[item] = name;
                     $scope.items.push(item);
+                    categories[name] = true;
                 }
+                $scope.categories.push({
+                    id: hitsJson.hits.hits[i]._id, 
+                    name: name, 
+                    items: categoryJson.items
+                });
             }
+            $scope.categoryNames = Object.keys(categories);
         }, function (error) {
             console.trace(error.message);
         });
 
         $scope.addRecipe = function() {
+            angular.forEach($scope.recipe.ingredients, function (ingredient) {
+                if (ingredient.selectedCategory) {
+                    // Do not add if it already belongs to a category
+                    if (!$scope.isCategorized(ingredient)) {
+                        $scope.categoryMap[ingredient.item] = ingredient.selectedCategory;
+                        $scope.items.push(ingredient.item);
+                        angular.forEach($scope.categories, function (category) {
+                            if (category.name === ingredient.selectedCategory) {
+                                category.items.push(ingredient.item);
+                            }
+                        });
+                    }
+                    // Do not save the selectedCategory in the database
+                    delete ingredient["selectedCategory"];
+                }
+            });
+
+            $scope.updateCategories();
+
             client.create({
                 index: 'test3',
                 type: 'recipe',
@@ -54,6 +82,27 @@ angular.module('addRecipesApp', ['elasticsearch', 'ui.bootstrap'])
                     $scope.isSaved = true;
                 }
                 //console.log("Response:" + JSON.stringify(response));
+            });
+        };
+
+        $scope.updateCategories = function() {
+            angular.forEach($scope.categories, function (category) {
+                client.update({
+                    index: 'test',
+                    type: 'category',
+                    id: category.id,
+                    body: {
+                        doc: {
+                            items: category.items
+                        }
+                    }
+                }, function (error, response) {
+                    if (error) {
+                        //console.trace(error.message);
+                        console.log("Error:" + error);
+                    }
+                    //console.log("Response:" + JSON.stringify(response));
+                });
             });
         };
 
@@ -73,7 +122,20 @@ angular.module('addRecipesApp', ['elasticsearch', 'ui.bootstrap'])
                     return $scope.categoryMap[ingredient.item];
                 }
             }
-            // set ingredient.isCategorized = false;
             return "None";
+        };
+
+        $scope.isCategorized = function(ingredient) {
+            if (ingredient) {
+                var item = $scope.categoryMap[ingredient.item];
+                if (item) {
+                    return true;
+                }
+            }
+            return false;
+        };
+
+        $scope.setSelectedCategory = function(ingredient, category) {
+            ingredient.selectedCategory = category;
         };
     });
