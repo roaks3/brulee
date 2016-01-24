5dbc816ba4a1300ef60a032de414478b511720c2
Ryan Oaks <roaks3@gmail.com>
2016-01-24 14:45:23 -0800

    Removed extraneous functionality from createList view

--- app/scripts/controllers/createList.js
+++ app/scripts/controllers/createList.js
@@ -7,7 +7,6 @@ angular.module('bruleeApp')
     groceryListService, ingredientService, recipeService) {
     
     $scope.recipes = [];
-
     $scope.refreshRecipes = function () {
       return recipeService.findAll()
         .then(function (data) {
@@ -16,7 +15,6 @@ angular.module('bruleeApp')
     };
 
     $scope.categories = [];
-
     $scope.refreshCategories = function () {
       $scope.errors = [];
       $scope.successMessage = null;
@@ -31,45 +29,13 @@ angular.module('bruleeApp')
         });
     };
 
-    $scope.groceryLists = [];
-
-    $scope.refreshGroceryLists = function () {
-      $scope.errors = [];
-      $scope.successMessage = null;
-
-      $scope.groceryLists = [];
-      groceryListService.findAll()
-        .then(function (data) {
-          $scope.groceryLists = data;
-        })
-        .catch(function (error) {
-          $scope.errors.push(error);
-        });
-    };
-
     $scope.refreshRecipes();
 
     $timeout(function () {
       $scope.refreshCategories();
     }, 1000);
 
-    $timeout(function () {
-      $scope.refreshGroceryLists();
-    }, 1500);
-
     $scope.shoppingList = [];
-
-    $scope.selectGroceryList = function (id) {
-      var groceryList = _.find($scope.groceryLists, 'id', id);
-      var selectedRecipeIds = _.map(groceryList.recipe_days, 'recipe_id');
-
-      _.each(selectedRecipeIds, function (recipeId) {
-        var recipe = _.find($scope.recipes, 'id', recipeId);
-        recipe._selected = true;
-      });
-
-      $scope.shoppingList = $filter('groceryListFilter')(groceryList);
-    };
 
     $scope.calculateShoppingList = function () {
       var selectedRecipes = _($scope.recipes)
