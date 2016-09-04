18dacabe9e6ecfdb76beb8a963cbe2cb7e5c6532
Ryan Oaks <roaks3@gmail.com>
2016-09-04 17:14:54 -0400

    Fix gulp revisioning index.html

--- gulpfile.js
+++ gulpfile.js
@@ -198,6 +198,7 @@ gulp.task('clean:dist', function (cb) {
 gulp.task('client:build', ['transpile', 'html', 'styles'], function () {
   var jsFilter = $.filter('**/*.js');
   var cssFilter = $.filter('**/*.css');
+  var revFilter = $.filter(['**/*', '!index.html']);
 
   return gulp.src(paths.views.main)
     .pipe($.useref({searchPath: [yeoman.app, '.tmp']}))
@@ -208,7 +209,9 @@ gulp.task('client:build', ['transpile', 'html', 'styles'], function () {
     .pipe(cssFilter)
     .pipe($.minifyCss({cache: true}))
     .pipe(cssFilter.restore())
+    .pipe(revFilter)
     .pipe($.rev())
+    .pipe(revFilter.restore())
     .pipe($.revReplace())
     .pipe(gulp.dest(yeoman.dist));
 });
