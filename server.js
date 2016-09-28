da88039c3a8587728940a0c290b0c15d20616463
Ryan Oaks <roaks3@gmail.com>
2016-09-28 18:50:13 -0400

    Fix server.js to work with html5 mode

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var express = require('express')
 var app = express();
+var path = require('path');
 
 app.use(express.static('dist'));
+
+// All routes should redirect to the index.html
+app.route('/*')
+  .get((req, res) => {
+    res.sendFile(path.resolve(path.normalize(__dirname) + '/dist/index.html'));
+  });
 
 app.listen(process.env.PORT || 3001);
