dd8edb93257c93fa15c600862766b779924493df
Ryan Oaks <roaks3@gmail.com>
2016-10-30 08:25:16 -0400

    Improve grocery list selection

--- app/scripts/components/groceryListSelect.js
+++ app/scripts/components/groceryListSelect.js
@@ -11,8 +11,25 @@ class GroceryListSelectCtrl {
     this.GroceryList
       .refreshAll()
       .then((data) => {
-        this.groceryLists = _.sortBy(data, 'week_start');
+        this.groceryLists = _.takeRight(_.sortBy(data, 'week_start'), 4);
       });
+  }
+
+  displayName (groceryList) {
+    if (!groceryList || !groceryList.week_start) {
+      return '';
+    }
+
+    let weekStartMoment = moment(groceryList.week_start, 'YYYY-MM-DD');
+    let weeksAgo = moment().diff(weekStartMoment, 'weeks');
+
+    if (weeksAgo === 0) {
+      return 'This Week';
+    } else if (weeksAgo === 1) {
+      return 'Last Week';
+    } else {
+      return weeksAgo + ' Weeks Ago';
+    }
   }
 
 }
