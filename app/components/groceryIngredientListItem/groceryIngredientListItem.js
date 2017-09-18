4368a7c53209e6a00d8a09b4858a44d440ff20e1
Ryan Oaks <roaks3@gmail.com>
2017-09-18 09:12:37 -0400

    Modify grocery lists to include amounts per recipe

--- app/components/groceryIngredientListItem/groceryIngredientListItem.js
+++ app/components/groceryIngredientListItem/groceryIngredientListItem.js
@@ -25,6 +25,11 @@ class GroceryIngredientListItemCtrl {
     this.isExpanded = !this.isExpanded;
   }
 
+  recipeAmount (recipe) {
+    const recipeIngredient = recipe.recipe_ingredients.find(ri => ri.ingredient_id === this.ingredient.id);
+    return recipeIngredient.amount + (recipeIngredient.unit ? ' ' + recipeIngredient.unit : '');
+  }
+
 }
 
 export default angular.module('components.groceryIngredientListItem', [selectedGroceryListStore])
