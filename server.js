37c4d1f57bb06ac20bd30b44ee4bc1145ee65f92
Ryan Oaks <roaks3@gmail.com>
2019-04-14 14:25:43 -0400

    Add react app for certain routes

--- server.js
+++ server.js
@@ -8,7 +8,14 @@ const api = require('./api/dist');
 app.use(bodyParser.json());
 app.use('/api', cors(), api.router);
 
+// Routes transitioned to the react app should redirect to the react index.html
+app.route('/react_app')
+  .get((req, res) => {
+    res.sendFile(path.resolve(path.normalize(__dirname) + '/react_app/build/index.html'));
+  });
+
 app.use(express.static('dist'));
+app.use(express.static('react_app/build'));
 
 // All routes should redirect to the index.html
 app.route('/*')
