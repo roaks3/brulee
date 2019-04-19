1bf582b3be14aabffc7fab1c9b69bfc078c44089
Ryan Oaks <roaks3@gmail.com>
2019-04-19 17:48:29 -0400

    Add nav to react app

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -8,6 +8,20 @@ class App extends Component {
     return (
       <Router>
         <div className="container">
+          <nav class="navbar-container">
+            <div class="navbar-header">
+              <a href="/createList" class="navbar-logo">
+                brulee
+              </a>
+            </div>
+            <ul class="navbar-main">
+              <li><a href="/createList">Create List</a></li>
+              <li><a href="/groceries">Grocery Lists</a></li>
+              <li><a href="/addRecipes">Add Recipes</a></li>
+              <li><a href="/ingredients">Ingredients</a></li>
+            </ul>
+          </nav>
+
           <Route path="/r/ingredient/:id" component={IngredientScreen} />
         </div>
       </Router>
